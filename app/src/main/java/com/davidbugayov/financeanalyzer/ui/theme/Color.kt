d3259158d070b693f52224b26b76ee215f4b2f1b
david0080 <davidbugayov@ya.ru>
2025-03-12 13:28:09 +0300

    ADDED COLOR TO RES

--- app/src/main/java/com/davidbugayov/financeanalyzer/ui/theme/Color.kt
+++ app/src/main/java/com/davidbugayov/financeanalyzer/ui/theme/Color.kt
@@ -1,107 +1,81 @@
 package com.davidbugayov.financeanalyzer.ui.theme
 
+import android.content.Context
 import androidx.compose.ui.graphics.Color
+import androidx.core.content.ContextCompat
+import com.davidbugayov.financeanalyzer.R
 
-// Primary colors
-val md_theme_light_primary = Color(0xFF2196F3)
-val md_theme_light_onPrimary = Color.White
-val md_theme_light_primaryContainer = Color(0xFFBBDEFB)
-val md_theme_light_onPrimaryContainer = Color(0xFF1976D2)
-val md_theme_dark_primary = Color(0xFF90CAF9)
-val md_theme_dark_onPrimary = Color.Black
-val md_theme_dark_primaryContainer = Color(0xFF1976D2)
-val md_theme_dark_onPrimaryContainer = Color(0xFFBBDEFB)
+class AppColors(context: Context) {
 
-// Secondary colors
-val md_theme_light_secondary = Color(0xFF03A9F4)
-val md_theme_light_onSecondary = Color.White
-val md_theme_light_secondaryContainer = Color(0xFFB3E5FC)
-val md_theme_light_onSecondaryContainer = Color(0xFF0288D1)
-val md_theme_dark_secondary = Color(0xFF81D4FA)
-val md_theme_dark_onSecondary = Color.Black
-val md_theme_dark_secondaryContainer = Color(0xFF0288D1)
-val md_theme_dark_onSecondaryContainer = Color(0xFFB3E5FC)
+    // Primary colors
+    val md_theme_light_primary = Color(ContextCompat.getColor(context, R.color.primary_light))
+    val md_theme_light_onPrimary = Color(ContextCompat.getColor(context, R.color.on_primary_light))
+    val md_theme_light_primaryContainer = Color(ContextCompat.getColor(context, R.color.primary_container_light))
+    val md_theme_light_onPrimaryContainer = Color(ContextCompat.getColor(context, R.color.on_primary_container_light))
+    val md_theme_dark_primary = Color(ContextCompat.getColor(context, R.color.primary_dark))
+    val md_theme_dark_onPrimary = Color(ContextCompat.getColor(context, R.color.on_primary_dark))
+    val md_theme_dark_primaryContainer = Color(ContextCompat.getColor(context, R.color.primary_container_dark))
+    val md_theme_dark_onPrimaryContainer = Color(ContextCompat.getColor(context, R.color.on_primary_container_dark))
 
-// Tertiary colors
-val md_theme_light_tertiary = Color(0xFF00BCD4)
-val md_theme_light_onTertiary = Color.White
-val md_theme_light_tertiaryContainer = Color(0xFFB2EBF2)
-val md_theme_light_onTertiaryContainer = Color(0xFF0097A7)
-val md_theme_dark_tertiary = Color(0xFF80DEEA)
-val md_theme_dark_onTertiary = Color.Black
-val md_theme_dark_tertiaryContainer = Color(0xFF0097A7)
-val md_theme_dark_onTertiaryContainer = Color(0xFFB2EBF2)
+    // Secondary colors
+    val md_theme_light_secondary = Color(ContextCompat.getColor(context, R.color.secondary_light))
+    val md_theme_light_onSecondary = Color(ContextCompat.getColor(context, R.color.on_secondary_light))
+    val md_theme_light_secondaryContainer = Color(ContextCompat.getColor(context, R.color.secondary_container_light))
+    val md_theme_light_onSecondaryContainer = Color(ContextCompat.getColor(context, R.color.on_secondary_container_light))
+    val md_theme_dark_secondary = Color(ContextCompat.getColor(context, R.color.secondary_dark))
+    val md_theme_dark_onSecondary = Color(ContextCompat.getColor(context, R.color.on_secondary_dark))
+    val md_theme_dark_secondaryContainer = Color(ContextCompat.getColor(context, R.color.secondary_container_dark))
+    val md_theme_dark_onSecondaryContainer = Color(ContextCompat.getColor(context, R.color.on_secondary_container_dark))
 
-// Error colors
-val md_theme_light_error = Color(0xFFF44336)
-val md_theme_light_onError = Color.White
-val md_theme_light_errorContainer = Color(0xFFFFCDD2)
-val md_theme_light_onErrorContainer = Color(0xFFD32F2F)
-val md_theme_dark_error = Color(0xFFEF9A9A)
-val md_theme_dark_onError = Color.Black
-val md_theme_dark_errorContainer = Color(0xFFD32F2F)
-val md_theme_dark_onErrorContainer = Color(0xFFFFCDD2)
+    // Tertiary colors
+    val md_theme_light_tertiary = Color(ContextCompat.getColor(context, R.color.tertiary_light))
+    val md_theme_light_onTertiary = Color(ContextCompat.getColor(context, R.color.on_tertiary_light))
+    val md_theme_light_tertiaryContainer = Color(ContextCompat.getColor(context, R.color.tertiary_container_light))
+    val md_theme_light_onTertiaryContainer = Color(ContextCompat.getColor(context, R.color.on_tertiary_container_light))
+    val md_theme_dark_tertiary = Color(ContextCompat.getColor(context, R.color.tertiary_dark))
+    val md_theme_dark_onTertiary = Color(ContextCompat.getColor(context, R.color.on_tertiary_dark))
+    val md_theme_dark_tertiaryContainer = Color(ContextCompat.getColor(context, R.color.tertiary_container_dark))
+    val md_theme_dark_onTertiaryContainer = Color(ContextCompat.getColor(context, R.color.on_tertiary_container_dark))
 
-// Background colors
-val md_theme_light_background = Color(0xFFFAFAFA)
-val md_theme_light_onBackground = Color(0xFF212121)
-val md_theme_dark_background = Color(0xFF121212)
-val md_theme_dark_onBackground = Color.White
+    // Error colors
+    val md_theme_light_error = Color(ContextCompat.getColor(context, R.color.error_light))
+    val md_theme_light_onError = Color(ContextCompat.getColor(context, R.color.on_error_light))
+    val md_theme_light_errorContainer = Color(ContextCompat.getColor(context, R.color.error_container_light))
+    val md_theme_light_onErrorContainer = Color(ContextCompat.getColor(context, R.color.on_error_container_light))
+    val md_theme_dark_error = Color(ContextCompat.getColor(context, R.color.error_dark))
+    val md_theme_dark_onError = Color(ContextCompat.getColor(context, R.color.on_error_dark))
+    val md_theme_dark_errorContainer = Color(ContextCompat.getColor(context, R.color.error_container_dark))
+    val md_theme_dark_onErrorContainer = Color(ContextCompat.getColor(context, R.color.on_error_container_dark))
 
-// Surface colors
-val md_theme_light_surface = Color.White
-val md_theme_light_onSurface = Color(0xFF212121)
-val md_theme_light_surfaceVariant = Color(0xFFF5F5F5)
-val md_theme_light_onSurfaceVariant = Color(0xFF757575)
-val md_theme_dark_surface = Color(0xFF1E1E1E)
-val md_theme_dark_onSurface = Color.White
-val md_theme_dark_surfaceVariant = Color(0xFF2D2D2D)
-val md_theme_dark_onSurfaceVariant = Color(0xFFBDBDBD)
+    // Background colors
+    val md_theme_light_background = Color(ContextCompat.getColor(context, R.color.background_light))
+    val md_theme_light_onBackground = Color(ContextCompat.getColor(context, R.color.on_background_light))
+    val md_theme_dark_background = Color(ContextCompat.getColor(context, R.color.background_dark))
+    val md_theme_dark_onBackground = Color(ContextCompat.getColor(context, R.color.on_background_dark))
 
-// Outline
-val md_theme_light_outline = Color(0xFFBDBDBD)
-val md_theme_dark_outline = Color(0xFF757575)
+    // Surface colors
+    val md_theme_light_surface = Color(ContextCompat.getColor(context, R.color.surface_light))
+    val md_theme_light_onSurface = Color(ContextCompat.getColor(context, R.color.on_surface_light))
+    val md_theme_light_surfaceVariant = Color(ContextCompat.getColor(context, R.color.surface_variant_light))
+    val md_theme_light_onSurfaceVariant = Color(ContextCompat.getColor(context, R.color.on_surface_variant_light))
+    val md_theme_dark_surface = Color(ContextCompat.getColor(context, R.color.surface_dark))
+    val md_theme_dark_onSurface = Color(ContextCompat.getColor(context, R.color.on_surface_dark))
+    val md_theme_dark_surfaceVariant = Color(ContextCompat.getColor(context, R.color.surface_variant_dark))
+    val md_theme_dark_onSurfaceVariant = Color(ContextCompat.getColor(context, R.color.on_surface_variant_dark))
 
-// Income and Expense colors
-val md_theme_light_income = Color(0xFF4CAF50)
-val md_theme_dark_income = Color(0xFF81C784)
-val md_theme_light_expense = Color(0xFFF44336)
-val md_theme_dark_expense = Color(0xFFE57373)
+    // Outline
+    val md_theme_light_outline = Color(ContextCompat.getColor(context, R.color.outline_light))
+    val md_theme_dark_outline = Color(ContextCompat.getColor(context, R.color.outline_dark))
 
-// Светлая тема
-val PrimaryLight = Color(0xFF1E88E5) // Синий
-val SecondaryLight = Color(0xFF26A69A) // Бирюзовый
-val TertiaryLight = Color(0xFF7E57C2) // Фиолетовый
+    // Income and Expense colors
+    val md_theme_light_income = Color(ContextCompat.getColor(context, R.color.income_light))
+    val md_theme_dark_income = Color(ContextCompat.getColor(context, R.color.income_dark))
+    val md_theme_light_expense = Color(ContextCompat.getColor(context, R.color.expense_light))
+    val md_theme_dark_expense = Color(ContextCompat.getColor(context, R.color.expense_dark))
 
-val BackgroundLight = Color(0xFFF5F5F5)
-val SurfaceLight = Color(0xFFFFFFFF)
-val OnSurfaceLight = Color(0xFF212121)
-
-val IncomeColorLight = Color(0xFF4CAF50) // Зеленый для доходов
-val ExpenseColorLight = Color(0xFFF44336) // Красный для расходов
-
-// Темная тема
-val PrimaryDark = Color(0xFF64B5F6) // Светло-синий
-val SecondaryDark = Color(0xFF4DB6AC) // Светло-бирюзовый
-val TertiaryDark = Color(0xFFB39DDB) // Светло-фиолетовый
-
-val BackgroundDark = Color(0xFF121212)
-val SurfaceDark = Color(0xFF1E1E1E)
-val OnSurfaceDark = Color(0xFFEEEEEE)
-
-val IncomeColorDark = Color(0xFF81C784) // Светло-зеленый для доходов
-val ExpenseColorDark = Color(0xFFE57373) // Светло-красный для расходов
-
-// Общие цвета
-val ErrorColor = Color(0xFFD32F2F)
-val SuccessColor = Color(0xFF388E3C)
-val WarningColor = Color(0xFFFFA000)
-val InfoColor = Color(0xFF1976D2)
-
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+    // Common colors
+    val errorColor = Color(ContextCompat.getColor(context, R.color.error))
+    val successColor = Color(ContextCompat.getColor(context, R.color.success))
+    val warningColor = Color(ContextCompat.getColor(context, R.color.warning))
+    val infoColor = Color(ContextCompat.getColor(context, R.color.info))
+}
