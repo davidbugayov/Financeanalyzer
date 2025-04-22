393a490364fd574844b6a76e97347457510d6431
david0080 <davidbugayov@ya.ru>
2025-04-22 11:17:05 +0300

    fix code / name
    Signed-off-by: david0080 <davidbugayov@ya.ru>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.davidbugayov.financeanalyzer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21
-        versionName = "2.6"
+        versionCode = 22
+        versionName = "2.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
