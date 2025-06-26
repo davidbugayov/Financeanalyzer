899d8b45ce22ecdbe6e2c5e19d3357ecc693bf53
david0080 <davidbugayov@ya.ru>
2025-06-26 22:22:41 +0300

    Add RuStoreUtils stub in utils module

---
+++ utils/src/main/java/com/davidbugayov/financeanalyzer/utils/RuStoreUtils.kt
@@ -0,0 +1,13 @@
+package com.davidbugayov.financeanalyzer.utils
+
+import android.content.Context
+
+/**
+ * Stub implementation of RuStoreUtils for library modules.
+ * For non-rustore flavors, this does nothing.
+ */
+object RuStoreUtils {
+    fun checkForUpdates(context: Context) {
+        // No-op stub for non-rustore flavors
+    }
+} 
