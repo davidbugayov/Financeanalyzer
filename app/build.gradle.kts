807e4d5a9daed30aa781062dd81993d026a37c46
david0080 <davidbugayov@ya.ru>
2025-07-03 14:57:07 +0300

    release: prepare 2.21.0 for F-Droid, update changelog and versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.davidbugayov.financeanalyzer"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.compileSdk.get().toInt()
-        versionCode = 39
-        versionName = "2.20.0"
+        versionCode = 40
+        versionName = "2.21.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
